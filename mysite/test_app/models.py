21377a35e41beb960a921171bc2161374b580ed8
Daniel <daniel.delrio@obf.ateneo.edu>
2019-09-17 18:07:47 +0800

    Add and migrate Question and Choice models

--- mysite/test_app/models.py
+++ mysite/test_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	date_published = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
