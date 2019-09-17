dd5535b583fa2fc1be46489afb8b67d6f7087713
Daniel <daniel.delrio@obf.ateneo.edu>
2019-09-17 18:25:30 +0800

    Add routes for adding question and choices

--- mysite/test_app/urls.py
+++ mysite/test_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
    path('questions/', views.questions_list, name='questions_list'),
    path('questions/<int:pk>/', views.choices_list, name='choices_list'),
+   path('questions/add/', views.questions_add, name='questions_add'),
+   path('choices/add/', views.choices_add, name='choices_add'),
 ]
--- mysite/test_app/views.py
+++ mysite/test_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Question, Choice
+from .forms import QuestionForm, ChoiceForm
+from django.shortcuts import redirect
 
 def questions_list(request):
 	questions = Question.objects.all()
@@ -11,3 +13,27 @@ def choices_list(request, pk):
 	choices = Choice.objects.filter(question=question)
 	context = {'question': question, 'choices' : choices}
 	return render(request, 'test_app/choices_list.html', context)
+
+def questions_add(request):
+	if request.method == 'POST':
+		form = QuestionForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('questions_list')
+	else:
+		form = QuestionForm()
+
+	context = {'form' : form}
+	return render(request, 'test_app/questions_add.html', context)
+
+def choices_add(request):
+	if request.method == 'POST':
+		form = ChoiceForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('choices_add')
+	else:
+		form = ChoiceForm()
+		
+	context = {'form' : form}
+	return render(request, 'test_app/choices_add.html', context)
