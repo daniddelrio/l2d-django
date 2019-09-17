68f97a38e46f078f16db6742d1057d3d4ee3a349
Daniel <daniel.delrio@obf.ateneo.edu>
2019-09-17 18:08:05 +0800

    Make routes for question and choices list

--- mysite/test_app/urls.py
+++ mysite/test_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#    path('', views.index, name='index'),
+   path('questions/', views.questions_list, name='questions_list'),
+   path('questions/<int:pk>/', views.choices_list, name='choices_list'),
 ]
--- mysite/test_app/views.py
+++ mysite/test_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Question, Choice
 
-"""
-def index(request):
-	return HttpResponse('Welcome to your first view!')
+def questions_list(request):
+	questions = Question.objects.all()
+	context = {'questions': questions}
+	return render(request, 'test_app/questions_list.html', context)
 
-"""
+def choices_list(request, pk):
+	question = Question.objects.filter(pk=pk).first()
+	choices = Choice.objects.filter(question=question)
+	context = {'question': question, 'choices' : choices}
+	return render(request, 'test_app/choices_list.html', context)
